b89852f486473ec2ccc4d27f501545831fbd8f43
andruasha <anvolko_1@edu.hse.ru>
2025-01-21 22:33:29 +0300

    Отрисовка угловых соединений

--- src/electric_circuit.py
+++ src/electric_circuit.py
@@ -5,14 +5,14 @@ import matplotlib.pyplot as plt
 
 class ElectricCircuit:
 
-    def __init__(self, nodes_num, branches_num, nodes_coords):
+    def __init__(self, nodes_num, branches_num, nodes):
 
+        self.nodes = nodes
         self.nodes_num = nodes_num
-        self.branches_num = branches_num
-        self.nodes_coords = nodes_coords
         self.nodes_connections = {}
+        self.branches_num = branches_num
 
-    def find_near_node(self, node_coords):
+    def find_near_node(self, nodes):
         near_nodes = []
         directions = [
             {'x': SCALE, 'y': 0},
@@ -22,30 +22,64 @@ class ElectricCircuit:
         ]
 
         for direction in directions:
-            target_coords = {'x': node_coords['x'] + direction['x'],
-                             'y': node_coords['y'] + direction['y']}
-            if target_coords in self.nodes_coords.values():
-                key = next((k for k, v in self.nodes_coords.items() if v == target_coords), None)
+            target_coords = {'x': nodes['x'] + direction['x'],
+                             'y': nodes['y'] + direction['y']}
+            if target_coords in self.nodes.values():
+                key = next((k for k, v in self.nodes.items() if v == target_coords), None)
                 if key:
                     near_nodes.append({key: target_coords})
 
         return near_nodes
 
     def create_nodes_connections(self):
-        for node, coords in self.nodes_coords.items():
-            near_nodes = self.find_near_node(coords)
+        def generate_neighbour_coords(node_coords, scale):
+            return {
+                'left': {'x': node_coords['x'] - scale, 'y': node_coords['y']},
+                'upper_left': {'x': node_coords['x'] - scale, 'y': node_coords['y'] + scale},
+                'upper': {'x': node_coords['x'], 'y': node_coords['y'] + scale},
+                'upper_right': {'x': node_coords['x'] + scale, 'y': node_coords['y'] + scale},
+                'right': {'x': node_coords['x'] + scale, 'y': node_coords['y']},
+                'down_right': {'x': node_coords['x'] + scale, 'y': node_coords['y'] - scale},
+                'down': {'x': node_coords['x'], 'y': node_coords['y'] - scale},
+                'down_left': {'x': node_coords['x'] - scale, 'y': node_coords['y'] - scale},
+            }
+
+        def add_connection_if_needed(coord1, coord2, missing_coord, key_prefix):
+            if (self.check_exist_node(coord1) and
+                    self.check_exist_node(coord2) and
+                    not self.check_exist_node(missing_coord)):
+                node1 = self.get_name_by_coords(coord1)
+                node2 = self.get_name_by_coords(coord2)
+                connection_key = f"{node1}->{node2}"
+                if connection_key not in self.nodes_connections:
+                    self.nodes_connections[connection_key] = [[coord1, missing_coord, coord2]]
+
+        for node_name, node_coords in self.nodes.items():
+            near_nodes = self.find_near_node(node_coords)
             for near_node in near_nodes:
                 for key, value in near_node.items():
-                    if (((node + '->' + key) not in self.nodes_connections) and
-                            ((key + '->' + node) not in self.nodes_connections)):
-                        self.nodes_connections[node + '->' + key] = [[coords, value]]
+                    connection_key = f"{node_name}->{key}"
+                    reverse_key = f"{key}->{node_name}"
+                    if connection_key not in self.nodes_connections and reverse_key not in self.nodes_connections:
+                        self.nodes_connections[connection_key] = [[node_coords, value]]
+
+            neighbours = generate_neighbour_coords(node_coords, SCALE)
+
+            add_connection_if_needed(neighbours['left'], neighbours['upper'],
+                                     neighbours['upper_left'], "ul")
+            add_connection_if_needed(neighbours['upper'], neighbours['right'],
+                                     neighbours['upper_right'], "ur")
+            add_connection_if_needed(neighbours['right'], neighbours['down'],
+                                     neighbours['down_right'], "dr")
+            add_connection_if_needed(neighbours['down'], neighbours['left'],
+                                     neighbours['down_left'], "dl")
 
     def get_num_of_connected_nodes(self, target_node):
         connected_nodes = []
 
-        for key in self.nodes_connections.keys():
-            if target_node in key.split('->'):
-                connected_nodes.append((set(key.split('->')) - {target_node}).pop())
+        for node_connection in self.nodes_connections.keys():
+            if target_node in node_connection.split('->'):
+                connected_nodes.append((set(node_connection.split('->')) - {target_node}).pop())
 
         return connected_nodes
 
@@ -56,11 +90,11 @@ class ElectricCircuit:
         plt.yticks(range(-13, 14, 1))
         plt.xlim(-13, 13)
         plt.ylim(-13, 13)
-        plt.axis('equal')
+        # plt.axis('equal')
 
-        for node, coords in self.nodes_coords.items():
+        for node, coords in self.nodes.items():
             plt.plot(coords['x'], coords['y'], 'ko')
-
+        print(self.nodes_connections)
         for key, value in self.nodes_connections.items():
             for connection in value:
                 for i in range(0, len(connection)):
@@ -69,13 +103,18 @@ class ElectricCircuit:
                                  [connection[i]['y'], connection[i + 1]['y']], 'k-')
         plt.show()
 
-    def get_coords_by_node(self, node):
-        for key, value in self.nodes_coords.items():
-            if key == node:
+    def get_coords_by_name(self, node_name):
+        for key, value in self.nodes.items():
+            if key == node_name:
                 return value
 
+    def get_name_by_coords(self, node_coords):
+        for key, value in self.nodes.items():
+            if value == node_coords:
+                return key
+
     def check_exist_node(self, target_coords):
-        for name, coords in self.nodes_coords.items():
+        for name, coords in self.nodes.items():
             if coords == target_coords:
                 return True
         return False
