6d0a1368c6e702f5996e5b413431ad81f39c31d8
andruasha <anvolko_1@edu.hse.ru>
2025-04-18 23:25:45 +0300

    Зачатки GUI

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import eel
+
 from src.active_dipole.main import generate_active_dipole_schemes_set
 from src.coupling_coefficient.main import generate_coupling_coefficient_schemes_set
 from src.direct_current.main import generate_direct_current_schemes_set
@@ -5,6 +7,37 @@ from src.alternating_current.main import generate_alternating_current_schemes_se
 from src.transient_processes.main import generate_transient_processes_schemes_set
 from src.active_quadripole.main import generate_active_quadripole_schemes_set
 from src.filter.main import generate_filter_schemes_set
+
+
+eel.init('gui')
+
+
+@eel.expose
+def generate_schemes_set(
+    lab_theme,
+    nodes_num,
+    branches_num,
+    voltage_sources,
+    current_sources,
+    resistors,
+    capacitors,
+    inductors,
+    filter_type,
+    scheme_type
+):
+    print("Тема:", lab_theme)
+    print("Узлы:", nodes_num)
+    print("Ветви:", branches_num)
+    print("Источники напряжения:", voltage_sources)
+    print("Источники тока:", current_sources)
+    print("Резисторы:", resistors)
+    print("Конденсаторы:", capacitors)
+    print("Индуктивности:", inductors)
+    print("Тип фильтра:", filter_type)
+    print("Тип схемы:", scheme_type)
+
+
+eel.start('index.html', size=(600, 500))
 
 
 # generate_active_dipole_schemes_set(
@@ -58,7 +91,7 @@ from src.filter.main import generate_filter_schemes_set
 #     inductors_num=2
 # )
 
-generate_filter_schemes_set(
-    scheme_type='G',
-    filter_type='LPF'
-)
+# generate_filter_schemes_set(
+#     scheme_type='G',
+#     filter_type='LPF'
+# )
